d653ca5a47f07d3db055a9ee428849f8cd6eaea4
thedistortedwajdan <wajdan.mohammad@gmail.com>
2025-04-12 20:22:13 +0500

    Refactor code formatting and update backend URL in QR code generation to use localhost

--- front-end-nextjs/src/app/page.js
+++ front-end-nextjs/src/app/page.js
@@ -1,22 +1,24 @@
-'use client'
+"use client";
 
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  const [url, setUrl] = useState('');
-  const [qrCodeUrl, setQrCodeUrl] = useState('');
+  const [url, setUrl] = useState("");
+  const [qrCodeUrl, setQrCodeUrl] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`http://qr-backend:8000/generate-qr/?url=${url}`);
+      const response = await axios.post(
+        `http://localhost:8000/generate-qr/?url=${url}`
+      );
       // const response = await axios.post(
       //   `http://backend:8000/generate-qr/?url=${url}` // backend is the docker container name
       // );
       setQrCodeUrl(response.data.qr_code_url);
     } catch (error) {
-      console.error('Error generating QR Code:', error.message);
+      console.error("Error generating QR Code:", error.message);
     }
   };
 
@@ -31,7 +33,9 @@ export default function Home() {
           placeholder="Enter URL like https://www.example.com"
           style={styles.input}
         />
-        <button type="submit" style={styles.button}>Generate QR Code</button>
+        <button type="submit" style={styles.button}>
+          Generate QR Code
+        </button>
       </form>
       {qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" style={styles.qrCode} />}
     </div>
@@ -41,44 +45,43 @@ export default function Home() {
 // Styles
 const styles = {
   container: {
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#121212',
-    color: 'white',
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#121212",
+    color: "white",
   },
   title: {
-    margin: '0',
-    lineHeight: '1.15',
-    fontSize: '4rem',
-    textAlign: 'center',
+    margin: "0",
+    lineHeight: "1.15",
+    fontSize: "4rem",
+    textAlign: "center",
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   input: {
-    padding: '10px',
-    borderRadius: '5px',
-    border: 'none',
-    marginTop: '20px',
-    width: '300px',
-    color: '#121212'
-
+    padding: "10px",
+    borderRadius: "5px",
+    border: "none",
+    marginTop: "20px",
+    width: "300px",
+    color: "#121212",
   },
   button: {
-    padding: '10px 20px',
-    marginTop: '20px',
-    border: 'none',
-    borderRadius: '5px',
-    backgroundColor: '#0070f3',
-    color: 'white',
-    cursor: 'pointer',
+    padding: "10px 20px",
+    marginTop: "20px",
+    border: "none",
+    borderRadius: "5px",
+    backgroundColor: "#0070f3",
+    color: "white",
+    cursor: "pointer",
   },
   qrCode: {
-    marginTop: '20px',
+    marginTop: "20px",
   },
 };
