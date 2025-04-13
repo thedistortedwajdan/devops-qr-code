733a17fae5bec9503ab2cc3baa17313cd6762e93
thedistortedwajdan <wajdan.mohammad@gmail.com>
2025-04-13 21:23:35 +0500

    Reorganize debug prints for clarity in QR code generation

--- api/main.py
+++ api/main.py
@@ -33,10 +33,6 @@ bucket_name = os.getenv("AWS_S3_BUCKET_NAME") # Add your bucket name here
 
 @app.post("/generate-qr/")
 async def generate_qr(url: str):
-    print(f"Received URL: {url}")
-    print(f"Bucket name: {bucket_name}")
-    print(f"File name: {file_name}")
-    print(f"Image byte array type: {type(img_byte_arr)}")
     # Generate QR Code
     qr = qrcode.QRCode(
         version=1,
@@ -57,6 +53,10 @@ async def generate_qr(url: str):
     # Generate file name for S3
     file_name = f"qr_codes/{url.split('//')[-1]}.png"
 
+    print(f"Received URL: {url}")
+    print(f"Bucket name: {bucket_name}")
+    print(f"File name: {file_name}")
+    print(f"Image byte array type: {type(img_byte_arr)}")
     try:
         # Upload to S3
         s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
