5eaab887c660595c54d2af7f3922ab343dd512f3
thedistortedwajdan <wajdan.mohammad@gmail.com>
2025-04-27 00:07:17 +0500

    testing 6

--- front-end-nextjs/src/app/page.js
+++ front-end-nextjs/src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `http://localhost:8000/generate-qr/?url=${url}`
+        `http://13.60.227.158:8000/generate-qr/?url=${url}`
       );
       setQrCodeUrl(response.data.qr_code_url);
     } catch (error) {
